83bfd6f3d7eea531cf776dd9636b7953423dc3f6
vaibhav <vaibhav.lohar.ds@ghrcemj.raisoni.net>
2024-09-19 07:46:19 +0530

    add new program

--- add.py
+++ add.py
@@ -1,1 +1,48 @@
-print("hellow world")
+# print("hello world!")
+# print("it is my favorite name!")
+
+# addition 
+
+a =2
+b =3
+print(a+b)
+
+
+# addition string
+
+a1 = "hello"
+
+b1 = "vaibhav"
+
+print(a1+" "+b1)
+
+# subtraction
+
+a2 = 5
+b2 = 3
+print(a2-b2)
+
+# multiplication
+
+a3 = 2
+b3 = 3
+print(a3*b3)
+
+# division
+
+a4 = 10
+b4 = 2
+print(a4/b4)
+
+# modulos
+
+a5 = 10
+b5 = 3
+print(a5%b5)
+
+# exponentiation
+
+a6 = 2
+b6 = 3
+print(a6**b6)
+
