6e94e506a97f1bdaddca386f21cb7e84aada2714
vaibhav <vaibhav.lohar.ds@ghrcemj.raisoni.net>
2024-09-19 20:49:01 +0530

    add new programs

---
+++ new.py
@@ -0,0 +1,50 @@
+val=input ("Enter the number")
+value_float=float(val)
+if (value_float%2==0):
+    print("the number is even")
+else:
+    print("the number is odd")
+
+
+print("this is for loop program ")
+
+
+lst = [1,2,3,4,5,6,7,8,]
+for i in lst:
+    print(i**2)
+
+
+
+print("this is while program")
+
+i=0
+while (i<=10):
+    print(i)
+    if (i==6):
+        break
+    i=i+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
