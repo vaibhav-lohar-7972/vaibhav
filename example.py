da380342cf91ad53e6f945e96b3fa0392e4abc9c
vaibhav <vaibhav.lohar.ds@ghrcemj.raisoni.net>
2024-09-19 08:14:03 +0530

    add new program file

---
+++ example.py
@@ -0,0 +1,42 @@
+# or oprator
+
+x = False
+y = True
+print(x or y)
+
+# and operator
+
+x = True
+y = False
+print(x and y)
+
+# not operator
+
+x = True
+print(not x)
+
+# equal operator
+
+x = 10
+y = 10
+print(x == y)
+
+# not equal operator
+
+x = 10
+y = 20
+print(x!= y)
+
+# less than operator
+
+x = 10
+y = 20
+print(x < y)
+
+# greater than operator
+
+x = 20
+y = 10
+print(x > y)
+
+# less than or equal to operator
